e65626e24f086f4d8b377d60e71f32f72cd27a4b
Benjamin Huang <coolconfucius@yahoo.com>
2016-02-16 23:28:33 -0800

    prep deploy

--- js/module.js
+++ js/module.js
@@ -6,13 +6,13 @@ app.constant('fbUrl', 'https://20160128.firebaseio.com/');
 
 app.config(function($stateProvider, $urlRouterProvider) {
   $stateProvider
-    .state('home', { url: '/', templateUrl: '/html/home.html', controller: 'mainCtrl' })
+    .state('home', { url: '/', templateUrl: 'html/home.html', controller: 'mainCtrl' })
 
     .state('user', { url: '/user', template: '<ui-view/>', abstract: true })
-    .state('user.login', { url: '/login', templateUrl: '/html/user.html', controller: 'userCtrl' })
-    .state('user.register', { url: '/register', templateUrl: '/html/user.html', controller: 'userCtrl' })
+    .state('user.login', { url: '/login', templateUrl: 'html/user.html', controller: 'userCtrl' })
+    .state('user.register', { url: '/register', templateUrl: 'html/user.html', controller: 'userCtrl' })
 
-    .state('user.profile', { url: '/profile', templateUrl: '/html/profile.html', controller: 'profileCtrl', 
+    .state('user.profile', { url: '/profile', templateUrl: 'html/profile.html', controller: 'profileCtrl', 
           onEnter: function($state, fbAuth) {
             console.log('getAuth:', fbAuth.$getAuth);
             if(!fbAuth.$getAuth()) {
